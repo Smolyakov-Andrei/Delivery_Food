135c6422370e202273691ea05ac32d5bd4a488b7
Smolyakov-Andrei <Chrisztw@mail.ru>
2025-09-18 17:04:00 +0700

    5 stage + vue

---
+++ delivery-vue/src/components/Footer.vue
@@ -0,0 +1,113 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="footer-box">
+        <router-link to="/" class="footer-logo logo">
+          <img src="../assets/images/logo.png" alt="logo" />
+        </router-link>
+        <div class="footer-nav">
+          <ul>
+            <li>
+              <a href="/for-restaurants" class="footer-link">Ресторанам</a>
+            </li>
+            <li>
+              <a href="/for-couriers" class="footer-link">Курьерам</a>
+            </li>
+            <li>
+              <a href="/press" class="footer-link">Пресс-центр</a>
+            </li>
+            <li>
+              <a href="/contacts" class="footer-link">Контакты</a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-social">
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="../assets/images/social/INSTAGRAM.png" alt="INSTAGRAM" />
+          </a>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="../assets/images/social/FACEBOOK.png" alt="FACEBOOK" />
+          </a>
+          <a href="https://vk.com" target="_blank" rel="noopener noreferrer">
+            <img src="../assets/images/social/VK.png" alt="VK" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.footer {
+  display: flex;
+  align-items: center;
+  min-height: 156px;
+  padding: 60px 0px;
+}
+
+.footer-box {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 36px;
+  flex-wrap: wrap;
+}
+
+.footer-nav {
+  flex-grow: 1;
+}
+
+.footer-nav ul {
+  display: flex;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  gap: 15px;
+}
+
+.footer-nav ul li {
+  color: #595959;
+  font-size: 18px;
+  font-weight: 400;
+}
+
+.footer-link {
+  color: inherit;
+  text-decoration: none;
+  transition: color 0.3s ease;
+}
+
+.footer-link:hover {
+  color: #000;
+}
+
+.footer-social {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 20px;
+}
+
+.footer-social a {
+  display: flex;
+  transition: opacity 0.3s ease;
+}
+
+.footer-social a:hover {
+  opacity: 0.7;
+}
+</style>
---
+++ delivery-vue/src/views/ProductView.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <section class="products">
+    <div class="container">
+      <div class="products-wrapper" id="goods-container">
+        <router-link
+          class="products-card"
+          v-for="goods in goodsArray"
+          :key="goods.id"
+          :to="`/rest?id=${goods.id}`"
+        >
+          <div class="products-card_image">
+            <img :src="getUrl(goods.image)" alt="rest-1" />
+          </div>
+          <div class="products-card_description">
+            <div class="products-card_description-row">
+              <h5 class="products-card_description--name">
+                {{ goods.title }}
+              </h5>
+            </div>
+            <div class="products-card_description-row">
+              <p class="products-card_description--text">
+                {{ goods.composition }}
+              </p>
+            </div>
+            <div class="products-card_description-row">
+              <div class="products-card_description-controls">
+                <button class="btn btn-primary">
+                  В корзину
+                  <img
+                    src="../assets/images/icons/shopping-cart-2.svg"
+                    alt="shopping-cart-2"
+                  />
+                </button>
+                <span class="products-card_description-controls--price"
+                  >{{ goods.price }} ₽</span
+                >
+              </div>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { goodsArray } from "../constants/goods";
+const getUrl = (name) => {
+  return new URL(`../assets/images/goods/${name}`, import.meta.url);
+};
+</script>
+
